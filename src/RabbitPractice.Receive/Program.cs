eba52a3d0f61c7c9fb9aa83c1fe84d7ec5860bdd
Rector <lampo1024@gmail.com>
2019-11-07 10:35:45 +0800

    Create producer and consumer console applications.

---
+++ src/RabbitPractice.Receive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitPractice.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
